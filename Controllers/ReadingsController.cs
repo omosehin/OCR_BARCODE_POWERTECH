123644f35c0c58d72ead432444fe40e57590e0c4
Abayomi <omosehin14@gmail.com>
2020-02-11 17:11:16 +0100

    Add project files.

---
+++ Controllers/ReadingsController.cs
@@ -0,0 +1,98 @@
+﻿using Manual_Validation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IronOcr;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Manual_Validation.Controllers
+{
+   
+    public class ReadingsController : ApiController
+    {
+        private static readonly string _oCRSpaceApi = ConfigurationManager.AppSettings["OCRSpaceApi"];
+        private static readonly string _oCRSpaceApiKey = ConfigurationManager.AppSettings["OCRSpaceApiKey"];
+        private static readonly HttpClient _client = new HttpClient();
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> AddReadings()
+        {
+            
+            var res = HttpContext.Current;
+            var httpContext = HttpContext.Current.Request;
+            var OCREngine = httpContext.Params["OCREngine"];
+            var ImageBase64 = httpContext.Params["ImageBase64"];
+            var MeterNumber = httpContext.Params["MeterNumber"];
+
+            MultipartFormDataContent multipartForm = new MultipartFormDataContent();
+
+            multipartForm.Add(new StringContent(ImageBase64), "Base64Image");
+            multipartForm.Add(new StringContent(OCREngine), "OcrEngine");
+            
+            _client.DefaultRequestHeaders.Add("apikey", _oCRSpaceApiKey);
+          
+            var response = await _client.PostAsync(_oCRSpaceApi, multipartForm);
+
+            string strContent = await response.Content.ReadAsStringAsync();
+
+            Rootobject ocrResult = JsonConvert.DeserializeObject<Rootobject>(strContent);
+
+            StringBuilder txtResult = new StringBuilder();
+            StringBuilder errResult = new StringBuilder();
+            var validateScannedConsumptionenergy = new Regex(@"^[0-9]*$"); //regex to validate only numbers
+
+            if (ocrResult.OCRExitCode == 1)
+            {
+                for (int i = 0; i < ocrResult.ParsedResults.Count(); i++)
+                {
+                    var scannedText = ocrResult.ParsedResults[i].ParsedText;
+                    if (!validateScannedConsumptionenergy.IsMatch(scannedText))
+                    {
+                        HttpResponseMessage errResponse = Request.CreateResponse(HttpStatusCode.BadRequest, "Energy consumption should only be scan numbers");
+                        return errResponse;
+                    }
+                    if (scannedText == "")
+                    {
+                        HttpResponseMessage emptyScanErrResponse = Request.CreateResponse(HttpStatusCode.BadRequest, "Energy consumption is not scanned very well");
+                        return emptyScanErrResponse;
+                    }
+
+                    txtResult.Append(scannedText);
+                }
+            }
+            else
+            {
+                errResult.Append("ERROR: " + strContent);
+            }
+
+
+            var MeterReadings = new MeterReading
+            {
+                MeterNumber = MeterNumber,
+                EnergyConsumption =Convert.ToString(txtResult),
+              //  WebId = Guid.NewGuid().ToString().Replace('-', '0').ToUpper()
+            };
+            using var _context = new DataContext();
+            _context.MeterReadings.Add(MeterReadings);
+            var save = _context.SaveChanges() > 1;
+            HttpResponseMessage myresponse = Request.CreateResponse(HttpStatusCode.OK,"Successfully saved");
+            return myresponse;
+
+            throw new Exception("Bad Request");
+        }
+    }
+}
---
+++ Scripts/main.js
@@ -0,0 +1,197 @@
+﻿/*!
+ * WebCodeCamJS 2.1.0 javascript Bar code and QR code decoder 
+ * Author: Tóth András
+ * Web: http://atandrastoth.co.uk
+ * email: atandrastoth@gmail.com
+ * Licensed under the MIT license
+ */
+
+let getMeterNumber;
+(function (undefined) {
+    "use strict";
+
+    function Q(el) {
+        if (typeof el === "string") {
+            var els = document.querySelectorAll(el);
+            return typeof els === "undefined" ? undefined : els.length > 1 ? els : els[0];
+        }
+        return el;
+    }
+    var txt = "innerText" in HTMLElement.prototype ? "innerText" : "textContent";
+    var scannerLaser = Q(".scanner-laser"),
+        imageUrl = new Q("#image-url"),
+        play = Q("#play"),
+        scannedImg = Q("#scanned-img"),
+        scannedQR = Q("#scanned-QR"),
+        grabImg = Q("#grab-img"),
+        decodeLocal = Q("#decode-img"),
+        pause = Q("#pause"),
+        stop = Q("#stop")
+        
+    var args = {
+        resultFunction: function (res) {
+            [].forEach.call(scannerLaser, function (el) {
+                fadeOut(el, 0.5);
+                setTimeout(function () {
+                    fadeIn(el, 0.5);
+                }, 300);
+            });
+            scannedImg.src = res.imgData;
+            scannedQR[txt] = res.format + ": " + res.code;
+            getMeterNumber =scannedQR[txt]
+            
+        },
+        getDevicesError: function (error) {
+            var p, message = "Error detected with the following parameters:\n";
+            for (p in error) {
+                message += p + ": " + error[p] + "\n";
+            }
+            alert(message);
+        },
+        getUserMediaError: function (error) {
+            var p, message = "Error detected with the following parameters:\n";
+            for (p in error) {
+                message += p + ": " + error[p] + "\n";
+            }
+            alert(message);
+        },
+        cameraError: function (error) {
+            var p, message = "Error detected with the following parameters:\n";
+            if (error.name == "NotSupportedError") {
+                var ans = confirm("Your browser does not support getUserMedia via HTTP!\n(see: https:goo.gl/Y0ZkNV).\n You want to see github demo page in a new window?");
+                
+            } else {
+                for (p in error) {
+                    message += p + ": " + error[p] + "\n";
+                }
+                alert(message);
+            }
+        },
+        cameraSuccess: function () {
+            grabImg.classList.remove("disabled");
+        }
+    };
+    var decoder = new WebCodeCamJS("#webcodecam-canvas").buildSelectMenu("#camera-select", "environment|back").init(args);
+    
+    play.addEventListener("click", function () {
+        if (!decoder.isInitialized()) {
+            scannedQR[txt] = "Scanning ...";
+        } else {
+            scannedQR[txt] = "Scanning ...";
+            decoder.play();
+        }
+    }, false);
+    
+    pause.addEventListener("click", function (event) {
+        scannedQR[txt] = "Paused";
+        decoder.pause();
+    }, false);
+    stop.addEventListener("click", function (event) {
+        scannedQR[txt] = "Stopped";
+        decoder.stop();
+    }, false);
+  
+  
+    Page.decodeLocalImage = function () {
+        if (decoder.isInitialized()) {
+            decoder.decodeLocalImage(imageUrl.value);
+        }
+        imageUrl.value = null;
+    };
+  
+
+    function fadeOut(el, v) {
+        el.style.opacity = 1;
+        (function fade() {
+            if ((el.style.opacity -= 0.1) < v) {
+                el.style.display = "none";
+                el.classList.add("is-hidden");
+            } else {
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+
+    function fadeIn(el, v, display) {
+        if (el.classList.contains("is-hidden")) {
+            el.classList.remove("is-hidden");
+        }
+        el.style.opacity = 0;
+        el.style.display = display || "block";
+        (function fade() {
+            var val = parseFloat(el.style.opacity);
+            if (!((val += 0.1) > v)) {
+                el.style.opacity = val;
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+    document.querySelector("#camera-select").addEventListener("change", function () {
+        if (decoder.isInitialized()) {
+            decoder.stop().play();
+        }
+    });
+}).call(window.Page = window.Page || {});
+
+
+
+/*For capturing meter Consumption */
+let showCamera = false;
+
+function displayCameraForEnergyConsuptionCapture() {
+    showCamera = true;
+    showCameraNow()
+}
+
+var my_camera = document.getElementById('my_camera')
+
+function showCameraNow() {
+    if (showCamera === true) {
+        Webcam.set({
+            width: 320,
+            height: 240,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
+        Webcam.attach(my_camera);
+    }
+}
+
+let getBase64;
+
+//< !--Code to handle taking the snapshot and displaying it locally-- >
+function take_snapshot() {
+  
+        Webcam.snap(function (data_uri) {
+            getBase64 = data_uri
+            document.getElementById('results').src = data_uri
+        });
+        Webcam.reset();
+    if (getMeterNumber != null) {
+        $("#save").removeClass('hidden')
+    }       
+}
+
+
+
+$("#save").click(function () {
+    let formData = new FormData();
+    formData.append("ImageBase64", getBase64)
+    formData.append("MeterNumber", getMeterNumber.split(':')[1])
+    formData.append("OCREngine", "2")
+
+    fetch('https://localhost:44311/api/Readings', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(response => {
+            $(".succMsg").append(response);
+        })
+        .catch(error => {
+            console.log(error)
+            $(".errMsg").append(error)
+            getMeterNumber = null
+            // console.log(error)
+        });
+})
